2c5e684a7a541595113b370f6ad647aa9dd37534
Nguyen Hoang Tu <82506580+toinguyenhoangtu@users.noreply.github.com>
2023-08-28 18:01:43 +0700

    add pgae

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,7 +8,7 @@ export const Header = (): JSX.Element => {
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
       <Wrapper className='h-[60px] flex justify-between items-center'>
         
-      </Wrapper>
+      </Wrapper>  
     </header>
   )
 }
